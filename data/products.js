61ac111c8b6c61e3e02c4d2bb10f51f87e6ac9f9
marciock <marciockalves@gmail.com>
2019-08-07 17:51:57 -0300

    ecommerce

---
+++ data/products.js
@@ -0,0 +1,184 @@
+const products=[
+    {
+        id:'1',
+        title:'Computador Desktop',
+        brand:'ICC',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        price:'2560,80',
+        img:'43679207_1GG.jpg'
+    },
+    {
+        id:'2',
+        title:'Fonte de energia para Iphone',
+        brand:'Magsafes',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        price:'200,00',
+        img:'55003179_1GG.jpg'
+    },
+    {
+        id:'3',
+        title:'Computador ALL-IN-ONE',
+        brand:'ICC',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        price:'5000,00',
+        img:'55264890_1GG.jpg'
+    },
+    {
+        id:'4',
+        title:'Tablet 7 Polegadas',
+        brand:'Samsung',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        price:'320,00',
+        img:'132475745_1GG.jpg'
+    },
+    {
+        id:'5',
+        title:'Notebook 15 Polegadas',
+        brand:'Samsung',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        price:'3250,00',
+        img:'133775934_1GG.png'
+    },
+    {
+        id:'6',
+        title:'AirBook 13 Polegadas',
+        brand:'Eapple',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        price:'25800,00',
+        img:'133785403_1GG.jpg'
+    },
+    {
+        id:'7',
+        title:'Notebook 16 Polegadas',
+        brand:'DELL',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        price:'4800,00',
+        img:'134393911_1GG.jpg'
+
+    },
+
+]
+
+const image_products=[
+    {
+        id:'1',
+        id_products:'1',
+        img:'43679207_1GG.jpg'
+    },
+    {
+        id:'2',
+        id_products:'1',
+        img:'43679207_2GG.jpg'
+    },
+    {
+        id:'3',
+        id_products:'1',
+        img:'43679207_3GG.jpg'
+    },
+    {
+        id:'4',
+        id_products:'1',
+        img:'43679207_4GG.jpg'
+    },
+    {
+        id:'5',
+        id_products:'2',
+        img:'55003179_1GG.jpg'
+    },
+    {
+        id:'6',
+        id_products:'2',
+        img:'55003179_2GG.jpg'
+    },
+    {
+        id:'7',
+        id_products:'2',
+        img:'55003179_3GG.jpg'
+    },
+    {
+        id:'8',
+        id_products:'3',
+        img:'55264890_1GG.jpg'
+    },
+    {
+        id:'9',
+        id_products:'4',
+        img:'132475745_1GG.jpg'
+    },
+    {
+        id:'10',
+        id_products:'4',
+        img:'132475745_2GG.jpg'
+    },
+    {
+        id:'11',
+        id_products:'4',
+        img:'132475745_4GG.jpg'
+    },
+    {
+        id:'12',
+        id_products:'4',
+        img:'132475745_7GG.jpg'
+    },
+    {
+        id:'13',
+        id_products:'5',
+        img:'133775934_1GG.png'
+    },
+    {
+        id:'14',
+        id_products:'5',
+        img:'133775934_4GG.jpg'
+    },
+    {
+        id:'15',
+        id_products:'5',
+        img:'133775934_5GG.jpg'
+    },
+    {
+        id:'16',
+        id_products:'5',
+        img:'133775934_6GG.jpg'
+    },
+    {
+        id:'17',
+        id_products:'6',
+        img:'133785403_1GG.jpg'
+    },
+    {
+        id:'18',
+        id_products:'6',
+        img:'133785403_2GG.jpg'
+    },
+    {
+        id:'19',
+        id_products:'7',
+        img:'134393911_1GG.jpg'
+    },
+    {
+        id:'20',
+        id_products:'7',
+        img:'134393911_2GG.jpg'
+    },
+    {
+        id:'21',
+        id_products:'7',
+        img:'134393911_4GG.jpg'
+    },
+    {
+        id:'22',
+        id_products:'7',
+        img:'134393911_5GG.jpg'
+    },
+    {
+        id:'23',
+        id_products:'7',
+        img:'134393911_6GG.jpg'
+    },
+    {
+        id:'24',
+        id_products:'7',
+        img:'134393911_7GG.jpg'
+    },
+
+]
